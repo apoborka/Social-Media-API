673ab5dfa680ea45c0d1a831944964dd5f6fc11e
apoborka <alex.poborka@gmail.com>
2025-03-25 17:42:09 -0400

    Added routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} from '../../controllers/userController.js';
+
+const router = express.Router();
+
+router.route('/')
+  .get(getUsers)
+  .post(createUser);
+
+router.route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+router.route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(removeFriend);
+
+export default router;
