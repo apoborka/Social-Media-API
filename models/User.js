a4b5d7e9ac622419f01baf5a1344fcc6157b462d
apoborka <alex.poborka@gmail.com>
2025-03-25 17:41:50 -0400

    Added models

---
+++ models/User.js
@@ -0,0 +1,47 @@
+import { Schema, model } from 'mongoose';
+
+// Schema for User
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match a valid email address'],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual to get the friend count
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+// Create the User model
+const User = model('User', userSchema);
+
+export default User;
